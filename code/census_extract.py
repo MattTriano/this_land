3be708798fd1f2491809ecbc462a293ffc097263
MattTriano <matt.triano@gmail.com>
2022-01-15 15:20:21 -0600

    Generalized state-line and county-line pulling functions.

--- code/census_extract.py
+++ code/census_extract.py
@@ -12,15 +12,17 @@ from utils import (
 )
 
 
-def extract_tiger_state_lines_2019(
-    project_root_dir: os.path = get_project_root_dir(), return_df: bool = True
+def extract_tiger_state_lines_from_one_year(
+    year: str,
+    project_root_dir: os.path = get_project_root_dir(),
+    return_df: bool = True,
 ) -> gpd.GeoDataFrame:
     data_documentation_url = (
         "https://www.census.gov/programs-surveys/geography/technical-documentation/"
-        + "complete-technical-documentation/tiger-geo-line.2019.html"
+        + f"complete-technical-documentation/tiger-geo-line.{year}.html"
     )
-    file_name = "census_tiger_state_lines_2019.zip"
-    url = "https://www2.census.gov/geo/tiger/TIGER2019/STATE/tl_2019_us_state.zip"
+    file_name = f"census_tiger_state_lines_{year}.zip"
+    url = f"https://www2.census.gov/geo/tiger/TIGER{year}/STATE/tl_{year}_us_state.zip"
     file_path = os.path.join(project_root_dir, "data_raw", file_name)
 
     return extract_file_from_url(
@@ -28,47 +30,19 @@ def extract_tiger_state_lines_2019(
     )
 
 
-def extract_tiger_county_lines_2019(
-    project_root_dir: os.path = get_project_root_dir(), return_df: bool = True
+def extract_tiger_county_lines_from_one_year(
+    year: str,
+    project_root_dir: os.path = get_project_root_dir(),
+    return_df: bool = True,
 ) -> pd.DataFrame:
     data_documentation_url = (
         "https://www.census.gov/programs-surveys/geography/technical-documentation/"
-        + "complete-technical-documentation/tiger-geo-line.2019.html"
+        + f"complete-technical-documentation/tiger-geo-line.{year}.html"
     )
-    file_name = "census_tiger_county_lines_2019.zip"
-    url = "https://www2.census.gov/geo/tiger/TIGER2019/COUNTY/tl_2019_us_county.zip"
-    file_path = os.path.join(project_root_dir, "data_raw", file_name)
-
-    return extract_file_from_url(
-        file_path=file_path, url=url, data_format="shp", return_df=return_df
+    file_name = f"census_tiger_county_lines_{year}.zip"
+    url = (
+        f"https://www2.census.gov/geo/tiger/TIGER{year}/COUNTY/tl_{year}_us_county.zip"
     )
-
-
-def extract_tiger_county_lines_2021(
-    project_root_dir: os.path = get_project_root_dir(), return_df: bool = True
-) -> pd.DataFrame:
-    data_documentation_url = (
-        "https://www.census.gov/programs-surveys/geography/technical-documentation/"
-        + "complete-technical-documentation/tiger-geo-line.2021.html"
-    )
-    file_name = "census_tiger_county_lines_2021.zip"
-    url = "https://www2.census.gov/geo/tiger/TIGER2021/COUNTY/tl_2021_us_county.zip"
-    file_path = os.path.join(project_root_dir, "data_raw", file_name)
-
-    return extract_file_from_url(
-        file_path=file_path, url=url, data_format="shp", return_df=return_df
-    )
-
-
-def extract_tiger_state_lines_2021(
-    project_root_dir: os.path = get_project_root_dir(), return_df: bool = True
-) -> gpd.GeoDataFrame:
-    data_documentation_url = (
-        "https://www.census.gov/programs-surveys/geography/technical-documentation/"
-        + "complete-technical-documentation/tiger-geo-line.2021.html"
-    )
-    file_name = "census_tiger_state_lines_2021.zip"
-    url = "https://www2.census.gov/geo/tiger/TIGER2021/STATE/tl_2021_us_state.zip"
     file_path = os.path.join(project_root_dir, "data_raw", file_name)
 
     return extract_file_from_url(
